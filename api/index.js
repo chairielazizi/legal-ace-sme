dc955c47888f57d0045c871f354c5e67371b1a78
chairielazizi <azizihr2000@gmail.com>
2025-01-28 14:35:29 +0800

    try deploy

--- api/index.js
+++
@@ -1,8 +0,0 @@
-const php = require("@vercel/php");
-
-module.exports = async (req, res) => {
-    const indexPath = "public/index.php";
-    const phpResponse = await php(indexPath);
-    res.set("Content-Type", "text/html");
-    res.send(phpResponse);
-};
